19eb19c6a7927cfc59e5b2c491ceea32255b367d
dariamorozova-dev <mdash.tv@gmail.com>
2020-09-08 15:40:30 +0200

    send button

---
+++ Students/Morozova_Daria/src/components/FloatingButton/FloatingButton.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Fab from '@material-ui/core/Fab';
+import SendIcon from '@material-ui/icons/Send';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+  extendedIcon: {
+    marginRight: theme.spacing(1),
+  },
+}));
+
+export default function FloatingActionButtons(props) {
+  const classes = useStyles();
+  const { send } = props;
+  return (
+    <div className={classes.root}>
+      <Fab color="primary" aria-label="add" onClick={ send }>
+        <SendIcon />
+      </Fab>
+    </div>
+  );
+}
