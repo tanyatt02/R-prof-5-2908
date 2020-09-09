34a05e90f803b8bb572247c9d397b37dda891c28
kellolo <87kell@gmail.com>
2020-09-09 22:18:06 +0200

    4

---
+++ PR/4/src/components/ChatList/ChatList.jsx
@@ -0,0 +1,47 @@
+
+        import './style.css';
+        import React, { Component, Fragment } from 'react';
+
+        import { Link } from 'react-router-dom';
+
+        import ChatsDialog from '../ChatsDialog/ChatsDialog.jsx';
+
+        import List  from '@material-ui/core/List/List';
+        import ListItem  from '@material-ui/core/ListItem/ListItem';
+        import ContentSend from '@material-ui/icons/Send';
+
+        export default class ChatList extends Component {
+            constructor(props) {
+                super(props);
+            }
+        
+            render() {
+        
+                return (
+                    <Fragment>
+                        <div className="ChatList d-flex flex-column">
+                            {/* <List>
+                            <ListItem primary = "Chat 1" />
+                            <ListItem primary = "Chat 2" />
+                            <ListItem primary = "Chat 3" />
+                            </List> */}
+                            <ul>
+                                <Link to = '/chat/1'>
+                                    <li>Chat 1</li>
+                                </Link>
+                                <Link to = '/chat/2'>
+                                    <li>Chat 2</li>
+                                </Link>
+                                <Link to = '/chat/3'>
+                                    <li>Chat 3</li>
+                                </Link>
+                            </ul>
+                            <div>
+                                <ChatsDialog />
+                            </div>
+                        </div>
+                    </Fragment>
+                )
+            }
+        }
+    
---
+++ PR/4/src/components/MessageField/MessageField.jsx
@@ -0,0 +1,107 @@
+
+        import './style.css';
+        import React, { Component, Fragment } from 'react';
+
+        import Message from '../Message/Message.jsx';
+
+        export default class MessageField extends Component {
+            constructor(props) {
+                super(props);
+                this.state = {
+                    text: '',
+                    messages: [
+                        {
+                            sender: 'Darth Vader',
+                            text: 'Hello'
+                        },
+                        {
+                            sender: 'Darth Vader',
+                            text: 'I am your father'
+                        },
+                        {
+                            sender: null,
+                            text: 'Hello'
+                        },
+                        {
+                            sender: null,
+                            text: 'Nooooooo'
+                        }
+                    ]
+                    // messages: {
+                    //     1: {
+                    //         sender: 'Darth Vader',
+                    //         text: 'Hello'
+                    //     },
+                    //     2: {
+                    //         sender: 'Darth Vader',
+                    //         text: 'I am your father'
+                    //     },
+                    //     3: {
+                    //         sender: null,
+                    //         text: 'Hello'
+                    //     },
+                    //     4: {
+                    //         sender: null,
+                    //         text: 'Nooooooo'
+                    //     }
+                    // }
+                }
+            }
+
+            handleChange = evt => {
+                if (evt.keyCode !== 13) {
+                    this.setState({ text: evt.target.value });
+                } else {
+                    this.sendMessage();
+                }
+                
+                // this.state.text = evt.target.value // NO REACTIVITY
+            }
+
+            sendMessage = () => {
+                this.setState({ 
+                    text: '',
+                    messages: [...this.state.messages, {
+                            sender: this.props.name,
+                            text: this.state.text
+                        } 
+                    ]
+                });
+            }
+
+            componentDidUpdate() {
+                //сделать, чтобы нам отвечал бот
+            }
+
+            render() {
+                let { messages } = this.state;
+
+                let contentArray = messages.map((msg, index) => {
+                    let { text, sender } = msg;
+                    return <Message text = { text } sender = { sender } key = { index }/>
+                });
+
+                // let contentArray = Object.keys(messages).map(key => {
+                //     let { text, sender } = messages[key];
+                //     return <Message text = { text } sender = { sender } key = { key }/>
+                // });
+        
+                return (
+                    <div className="d-flex flex-column">
+                        <div>
+                            { contentArray }
+                        </div>
+                        <div className="controls d-flex">
+                            <input 
+                                type="text" 
+                                value = { this.state.text }
+                                onChange = { this.handleChange }
+                                onKeyUp = { this.handleChange }
+                            />
+                            <button onClick = { this.sendMessage }>Send</button>
+                        </div>
+                    </div>
+                )
+            }
+        }
+    
