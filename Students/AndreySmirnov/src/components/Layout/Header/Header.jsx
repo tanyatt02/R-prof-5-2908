721b308798af343909264c89c6a2d7f3878e90f0
AndreySmir <smirnov.andrey.n@gmail.com>
2020-09-09 14:04:31 +0300

    Lesson _3_Home_Work

---
+++ Students/AndreySmirnov/src/components/Layout/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = (props) => {
+
+    return (
+        <div className='header'>
+            <span>Welcome to the chat-messenger</span>
+        </div>
+    )
+}
+
+export default Header
