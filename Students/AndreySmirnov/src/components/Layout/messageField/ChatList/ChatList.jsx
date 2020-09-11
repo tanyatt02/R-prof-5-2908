85ce2dfe25c586ce1224d60ab26990194f404438
AndreySmir <smirnov.andrey.n@gmail.com>
2020-09-12 00:25:40 +0300

    Lesson_4_Home_Work (Not completed)

--- Students/AndreySmirnov/src/components/Layout/messageField/ChatList/ChatList.jsx
+++ Students/AndreySmirnov/src/components/Layout/messageField/ChatList/ChatList.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import Avatar from 'material-ui/Avatar';
 import {List, ListItem} from 'material-ui/List';
 import Subheader from 'material-ui/Subheader';
-import Divider from 'material-ui/Divider';
 import CommunicationChatBubble from 'material-ui/svg-icons/communication/chat-bubble';
 
 const ChatList = () => (
