b948e6d64e687a524c48434729615e3ab6840c31
dariamorozova-dev <mdash.tv@gmail.com>
2020-09-14 16:24:00 +0200

    new Input comp

--- Students/Morozova_Daria/src/components/Input/Input.jsx
+++ Students/Morozova_Daria/src/components/Input/Input.jsx
@@ -1,46 +1,46 @@
+import './style.css';
 
-        import './style.css';
-        import React from 'react';
+import React, { Component } from 'react';
 
-        import {TextField} from '@material-ui/core';
-        import FloatingActionButton from '../FloatingButton/FloatingButton.jsx';
+import {TextField} from '@material-ui/core';
+import FloatingActionButton from '../FloatingButton/FloatingButton.jsx';
 
-        export default class Input extends React.Component {
-            constructor(props) {
-                super(props);
-                this.state = {
-                    text: ''
-                }
-            }
-
-            handleChange = event => {
-                if (event.keyCode !== 13) {
-                    this.setState({ text: event.target.value });
-                } else {
-                    this.sendMessage();
-                }                
-            }
-        
-            render() {
-                // let { some } = this.state;
-        
-                return (
-                    <div className="controls d-flex justify-content-end align-items-baseline" style={ {width: '100%', display: 'flex'} }>
-                        <TextField
-                            name="input"
-                            fullWidth={ true }
-                            placeholder="Type your message here"
-                            style={ {fontSize: '22px', margin: '0 0 20px 0', width: '80%'} }
-                            onChange={ this.handleChange }
-                            onKeyUp={ this.handleChange }
-                            value={ this.state.text }
-                        />
-
-                        <FloatingActionButton
-                            send={ this.props.send(text) }
-                        ></FloatingActionButton>
-                    </div>
-                )
-            }
+export default class InputComp extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: ''
         }
-    
+    }
+
+    handleChange = evt => {
+        if (evt.keyCode !== 13) {
+            this.setState({ text: evt.target.value });
+        } else {
+            this.props.send(this.state.text);
+        }
+    }
+
+    sendM = () => {
+        this.props.send(this.state.text);
+    }
+
+    render() {
+        let { text } = this.state;
+
+        return (
+            <div className="controls d-flex justify-content-end align-items-baseline" style={ {width: '100%', display: 'flex'} }>
+                <TextField
+                    name="input"
+                    fullWidth={ true }
+                    placeholder="Type your message here"
+                    style={ {fontSize: '22px', margin: '0 0 20px 0', width: '80%'} }
+                    onChange={ this.handleChange }
+                    onKeyUp={ this.handleChange }
+                    value={ this.state.text }
+                        />
+                <FloatingActionButton onClick = { this.sendM }>Send</FloatingActionButton>
+            </div>
+        )
+    }
+}
