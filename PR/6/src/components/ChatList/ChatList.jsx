427c9ef3ccb7365af31dd1de0d78db7a458995ce
kellolo <87kell@gmail.com>
2020-09-19 10:00:32 +0200

    add 6 proj

---
+++ PR/6/src/components/ChatList/ChatList.jsx
@@ -0,0 +1,34 @@
+
+import './style.css';
+import React, { Component, Fragment } from 'react';
+
+import { Link } from 'react-router-dom';
+
+import ChatsDialog from '../ChatsDialog/ChatsDialog.jsx';
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+
+import Item from '../ChatListItem/ChatListItem.jsx';
+
+export default class ChatList extends Component {
+    render() {
+        return (
+            <Fragment>
+                <div className="ChatList d-flex flex-column">
+                    <List>
+                        <Link to = '/chat/1'>
+                            <Item name="Chat 1" />
+                        </Link>
+                        <Link to = '/chat/2'>
+                            <Item name="Chat 2" />
+                        </Link>
+                    </List>
+                    <div>
+                        <ChatsDialog />
+                    </div>
+                </div>
+            </Fragment>
+        )
+    }
+}
+    
