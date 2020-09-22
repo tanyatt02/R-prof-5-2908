d05869d3f350305a139ef7da2aab44ffba834aa4
superlesik <superlesik@inbox.ru>
2020-09-22 13:24:11 +0300

    /

--- Students/LebedevaOlesya/src/middlewares/botMiddleware.js
+++ Students/LebedevaOlesya/src/middlewares/botMiddleware.js
@@ -1,11 +1,11 @@
-import { SEND_MESSAGE } from "../store/actions/messageActions.js";
+import { SEND_MESSAGE, sendMessage } from "../store/actions/messageActions.js";
 
 export default store => next => (action) => {
    switch (action.type) {
        case SEND_MESSAGE:
            if (action.sender === 'Me') {
             setTimeout(() => store.dispatch(
-                sendMessage(Object.keys(store.getState().messageReducer.messages).length + 1,
+                sendMessage(Object.keys(store.getState().msgReducer.messages).length + 1,
                 'Не приставай ко мне, я робот!', 'bot', action.chatId)), 
                 1000)
            }
