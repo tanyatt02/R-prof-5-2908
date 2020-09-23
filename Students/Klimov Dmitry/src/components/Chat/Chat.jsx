0703a9e216079358e558a4ea50b23360ec85f824
hamsterRus <hhomyak@mail.ru>
2020-09-23 14:50:14 +0300

    Дз 4 и 5

--- Students/Klimov Dmitry/src/components/Chat/Chat.jsx
+++ Students/Klimov Dmitry/src/components/Chat/Chat.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+
+import { Link } from 'react-router-dom';
+
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
@@ -19,46 +22,21 @@ const useStyles = makeStyles((theme) => ({
 
 export default function CheckboxList() {
   const classes = useStyles();
-  const [checked, setChecked] = React.useState([0]);
-
-  const handleToggle = (value) => () => {
-    const currentIndex = checked.indexOf(value);
-    const newChecked = [...checked];
-
-    if (currentIndex === -1) {
-      newChecked.push(value);
-    } else {
-      newChecked.splice(currentIndex, 1);
-    }
-
-    setChecked(newChecked);
-  };
 
   return (
     <List className={classes.root}>
-      {[0, 1, 2, 3].map((value) => {
-        const labelId = `checkbox-list-label-${value}`;
-
-        return (
-          <ListItem key={value} role={undefined} dense button onClick={handleToggle(value)}>
-            <ListItemIcon>
-              <Checkbox
-                edge="start"
-                checked={checked.indexOf(value) !== -1}
-                tabIndex={-1}
-                disableRipple
-                inputProps={{ 'aria-labelledby': labelId }}
-              />
-            </ListItemIcon>
-            <ListItemText id={labelId} primary={`Line item ${value + 1}`} />
-            <ListItemSecondaryAction>
-              <IconButton edge="end" aria-label="comments">
-                <CommentIcon />
-              </IconButton>
-            </ListItemSecondaryAction>
-          </ListItem>
-        );
-      })}
+      <Link to = '/chat/1'>
+        <ListItem button >Chat 1
+        </ListItem>
+      </Link> 
+      <Link to = '/chat/2'>
+        <ListItem button > Chat 2
+        </ListItem>
+      </Link>
+      <Link to = '/chat/3'>
+        <ListItem button > Chat 3
+        </ListItem>
+      </Link>   
     </List>
   );
 }
